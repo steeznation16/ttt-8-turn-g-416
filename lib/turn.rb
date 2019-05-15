88326ea0b33e864903e41ac4e2ec83bd496a44ef
Andrew Wester <awester17@gmail.com>
2019-05-15 17:38:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,10 @@
-def display_board
-  puts 
-end
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+  end
 
 def valid_move?
   
